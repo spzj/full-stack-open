2039c4539aad3239f0aa0bd813c45bf226cf11f3
spzj <54446335+spzj@users.noreply.github.com>
2024-05-23 04:15:27 +0800

    feat(phonebook): fetch persons from json-server
    exercise 2.11

--- Part 2 - Communicating with server/phonebook/.eslintrc.cjs
+++ Part 2 - Communicating with server/phonebook/.eslintrc.cjs
@@ -2,21 +2,48 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:import/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: {
+    "import/resolver": {
+      node: {
+        paths: ["src"],
+        extensions: [".js", ".jsx", ".ts", ".d.ts", ".tsx"],
+      },
+    },
+    react: { version: "18.2" },
+  },
+  plugins: ["import", "react-refresh"],
   rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+        ],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+    "react/jsx-no-target-blank": "off",
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
-    'react/prop-types': 0
+    "react/prop-types": 0,
   },
-}
+};
--- Part 2 - Communicating with server/phonebook/src/App.jsx
+++ Part 2 - Communicating with server/phonebook/src/App.jsx
@@ -1,20 +1,22 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
-
+  const [persons, setPersons] = useState([]);
   const [filter, setFilter] = useState("");
   const [filteredPersons, setFilteredPersons] = useState(persons);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -34,9 +36,9 @@ const App = () => {
       return;
     }
 
-    if (!/^[0-9+\-\+\(\)]+$/.test(newNumber)) {
+    if (!/^[0-9\s()+-]+$/.test(newNumber)) {
       alert(
-        "number field can only contain contain digits [0-9], dash [-], plus [+] and parentheses [()]"
+        "number field can only contain contain digits [0-9], dash [-], plus [+], parentheses [()] and whitespaces."
       );
       return;
     }
