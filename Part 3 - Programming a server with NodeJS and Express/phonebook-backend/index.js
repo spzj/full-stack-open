e6c34a6c72cca61b8f3cc142632eb92426333710
spzj <54446335+spzj@users.noreply.github.com>
2024-05-26 02:06:49 +0800

    refactor(phonebook-backend): change api methods to access MongoDB database
    exercises 3.13 - 3.14

--- Part 3 - Programming a server with NodeJS and Express/phonebook-backend/index.js
+++ Part 3 - Programming a server with NodeJS and Express/phonebook-backend/index.js
@@ -1,13 +1,12 @@
+require("dotenv").config();
+
 const cors = require("cors");
 const express = require("express");
 const morgan = require("morgan");
 
+const Person = require("./models/person");
+
 const app = express();
-
-app.use(express.json()); // Middleware to parse JSON data
-app.use(cors()); // Middleware to allow cross-origin requests
-app.use(express.static('dist'))
-
 const logger = morgan(function (tokens, req, res) {
   // default tiny format
   const items = [
@@ -23,96 +22,60 @@ const logger = morgan(function (tokens, req, res) {
   return items + (req.method === "POST" ? ` ${JSON.stringify(req.body)}` : "");
 });
 
+app.use(cors()); // Middleware to allow cross-origin requests
+app.use(express.json()); // Middleware to parse JSON data
+app.use(express.static("dist")); // Middleware to serve static files from dist directory
 app.use(logger); // Middleware to log requests
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-app.get("/info", (request, response) => {
-  response.send(
-    "<p>Phonebook has info for " +
-      persons.length +
-      " people</p>" +
-      "<p>" +
-      new Date() +
-      "</p>"
-  );
-});
+// app.get("/info", (request, response) => {
+//   response.send(
+//     "<p>Phonebook has info for " +
+//       persons.length +
+//       " people</p>" +
+//       "<p>" +
+//       new Date() +
+//       "</p>"
+//   );
+// });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((p) => p.id === id);
-
-  if (person) {
+  Person.findById(request.params.id).then((person) => {
     response.json(person);
-  } else {
-    response.status(404).end(); // 404 Not Found
-  }
+  });
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
+// app.delete("/api/persons/:id", (request, response) => {
+//   const id = Number(request.params.id);
+//   persons = persons.filter((person) => person.id !== id);
 
-  response.status(204).end(); // 204 No Content
-});
-
-const generateId = () => {
-  const randomId = Math.floor(Math.random() * 1000000);
-  return randomId;
-};
+//   response.status(204).end(); // 204 No Content
+// });
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
   if (!body.name || !body.number) {
     // 400 Bad Request
-    return response.status(400).json({
-      error: "name and/or number missing.",
-    });
+    return response.status(400).json({ error: "name or number missing" });
   }
 
-  if (persons.find((p) => p.name === body.name)) {
-    return response.status(400).json({
-      error: "name already exists in the phonebook.",
-    });
-  }
-
-  const person = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
+  });
 
-  persons = persons.concat(person);
-  response.json(person);
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
