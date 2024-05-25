92fd114f7beff63d6862f75f7a8eb5fe61a8e042
spzj <54446335+spzj@users.noreply.github.com>
2024-05-25 08:52:42 +0800

    feat(phonebook-backend): allow addition of entries through HTTP POST
    exercises 3.5 - 3.6

--- Part 3 - Programming a server with NodeJS and Express/phonebook-backend/index.js
+++ Part 3 - Programming a server with NodeJS and Express/phonebook-backend/index.js
@@ -59,6 +59,37 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end(); // 204 No Content
 });
 
+const generateId = () => {
+  const randomId = Math.floor(Math.random() * 1000000);
+  return randomId;
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    // 400 Bad Request
+    return response.status(400).json({
+      error: "name and/or number missing.",
+    });
+  }
+
+  if (persons.find((p) => p.name === body.name)) {
+    return response.status(400).json({
+      error: "name already exists in the phonebook.",
+    });
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+  response.json(person);
+});
+
 const PORT = 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
