89a5d98c2baef896eed30fc88161fc653a995f45
spzj <54446335+spzj@users.noreply.github.com>
2024-05-27 14:07:12 +0800

    refactor(bloglist): create separate modules for application
    exercise 4.2

--- Part 4 - Testing Express servers, user administration/bloglist/index.js
+++ Part 4 - Testing Express servers, user administration/bloglist/index.js
@@ -1,29 +1,7 @@
-require('dotenv').config()
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const express = require('express')
-const cors = require('cors')
-
-const Blog = require('./models/blog')
-
-const app = express()
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
--- Part 4 - Testing Express servers, user administration/bloglist/models/blog.js
+++ Part 4 - Testing Express servers, user administration/bloglist/models/blog.js
@@ -1,17 +1,5 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-const url = process.env.MONGODB_URI
-
-mongoose
-  .connect(url)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
@@ -20,5 +8,13 @@ const blogSchema = new mongoose.Schema({
 })
 
 mongoose.SchemaTypes.String.set('trim', true)
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
 
 module.exports = mongoose.model('Blog', blogSchema)
