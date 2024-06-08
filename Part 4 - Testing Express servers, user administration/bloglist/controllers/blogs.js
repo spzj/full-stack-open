e8140cfd3bf78b1d4bd48dcc34bef2dc55ec9454
spzj <54446335+spzj@users.noreply.github.com>
2024-06-08 16:00:36 +0800

    feat(bloglist): add delete and put for api and update blog schema
    4.9, 4.11* - 4.14

--- Part 4 - Testing Express servers, user administration/bloglist/controllers/blogs.js
+++ Part 4 - Testing Express servers, user administration/bloglist/controllers/blogs.js
@@ -12,11 +12,49 @@ blogsRouter.post('/', async (request, response) => {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
+    likes: body.likes,
   })
 
   const savedBlog = await blog.save()
   response.status(201).json(savedBlog)
+})
+
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog)
+  } else {
+    response.status(404).end()
+  }
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  const deletedBlog = await Blog.findByIdAndDelete(request.params.id)
+
+  if (deletedBlog) {
+    response.status(204).end()
+  } else {
+    response.status(404).end()
+  }
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const { title, author, url, likes } = request.body
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.id,
+    { title, author, url, likes },
+    {
+      new: true,
+      runValidators: true,
+      context: 'query',
+    }
+  )
+
+  if (updatedBlog) {
+    response.status(200).json(updatedBlog)
+  } else {
+    response.status(404).end()
+  }
 })
 
 module.exports = blogsRouter
