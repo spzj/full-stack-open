264027b25d3ad7aca74491be9f511e0a8c69f2ef
spzj <54446335+spzj@users.noreply.github.com>
2024-06-09 11:15:25 +0800

    feat(bloglist): add token-based authentication for post requests
    exercises 4.17 - 4.19

--- Part 4 - Testing Express servers, user administration/bloglist/controllers/blogs.js
+++ Part 4 - Testing Express servers, user administration/bloglist/controllers/blogs.js
@@ -1,15 +1,31 @@
+const config = require('../utils/config')
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const { title, author, url, likes, userId } = request.body
-  const user = await User.findById(userId)
+  const { title, author, url, likes } = request.body
+
+  const decodedToken = jwt.verify(getTokenFrom(request), config.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
   const blog = new Blog({
     title,
     author,
--- Part 4 - Testing Express servers, user administration/bloglist/utils/config.js
+++ Part 4 - Testing Express servers, user administration/bloglist/utils/config.js
@@ -1,12 +1,15 @@
 require('dotenv').config()
 
-const PORT = process.env.PORT
 const MONGODB_URI =
   process.env.NODE_ENV === 'test'
     ? process.env.TEST_MONGODB_URI
     : process.env.MONGODB_URI
 
+const PORT = process.env.PORT
+const SECRET = process.env.SECRET
+
 module.exports = {
   MONGODB_URI,
-  PORT
+  PORT,
+  SECRET,
 }
