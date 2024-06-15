a66b32c546ced8e6dd4c25b16acadeecef26d735
spzj <54446335+spzj@users.noreply.github.com>
2024-06-15 21:15:08 +0800

    feat(bloglist-frontend): update like count when like button is clicked
    exercises 5.8, 5.9

--- Part 5 - Testing React apps/bloglist-frontend/src/services/blogs.js
+++ Part 5 - Testing React apps/bloglist-frontend/src/services/blogs.js
@@ -7,18 +7,23 @@ const setToken = (newToken) => {
   token = `Bearer ${newToken}`;
 };
 
+const config = () => ({
+  headers: { Authorization: token },
+});
+
 const getAll = () => {
   const request = axios.get(baseUrl);
   return request.then((response) => response.data);
 };
 
 const create = async (newObject) => {
-  const config = {
-    headers: { Authorization: token },
-  };
-
-  const response = await axios.post(baseUrl, newObject, config);
+  const response = await axios.post(baseUrl, newObject, config());
   return response.data;
 };
 
-export default { setToken, getAll, create };
+const update = async (id, newObject) => {
+  const response = await axios.put(`${baseUrl}/${id}`, newObject, config());
+  return response.data;
+};
+
+export default { setToken, getAll, create, update };
