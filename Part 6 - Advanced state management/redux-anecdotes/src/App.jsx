06d7fd2fb68c0098403b94dd3156ffa34e191770
spzj <54446335+spzj@users.noreply.github.com>
2024-06-18 19:21:40 +0800

    refactor(redux-anecdotes): add action creators and components
    exercises 6.6 - 6.8

--- Part 6 - Advanced state management/redux-anecdotes/src/App.jsx
+++ Part 6 - Advanced state management/redux-anecdotes/src/App.jsx
@@ -1,42 +1,12 @@
-import { useSelector, useDispatch } from 'react-redux';
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList';
 
 const App = () => {
-  const anecdotes = useSelector((state) => state);
-  const dispatch = useDispatch();
-
-  const vote = (id) => {
-    dispatch({ type: 'VOTE', payload: { id } });
-  };
-
-  const addAnecdote = (event) => {
-    event.preventDefault();
-    console.log(event);
-    const anecdote = event.target.anecdote.value;
-    event.target.anecdote.value = '';
-    dispatch({ type: 'CREATE', payload: { anecdote } });
-  };
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes
-        .sort((a, b) => b.votes - a.votes)
-        .map((anecdote) => (
-          <div key={anecdote.id}>
-            <div>{anecdote.content}</div>
-            <div>
-              has {anecdote.votes}
-              <button onClick={() => vote(anecdote.id)}>vote</button>
-            </div>
-          </div>
-        ))}
-      <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div>
-          <input name='anecdote' />
-        </div>
-        <button>create</button>
-      </form>
+      <AnecdoteList />
+      <AnecdoteForm/>
     </div>
   );
 };
--- Part 6 - Advanced state management/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ Part 6 - Advanced state management/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -19,6 +19,14 @@ const asObject = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject);
 
+export const vote = (id) => {
+  return { type: 'VOTE', payload: { id } };
+};
+
+export const createAnecdote = (anecdote) => {
+  return { type: 'CREATE', payload: { anecdote } };
+};
+
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state);
   console.log('action', action);
