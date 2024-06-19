97cf079194503dc6f25b0b60788fc85c8cf58433
spzj <54446335+spzj@users.noreply.github.com>
2024-06-19 16:59:03 +0800

    feat(redux-anecdotes): add json server
    exercises 6.14, 6.15

--- Part 6 - Advanced state management/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ Part 6 - Advanced state management/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,16 +1,19 @@
 import { useDispatch } from 'react-redux';
 import { createAnecdote } from '../reducers/anecdoteReducer';
 import { setNotification } from '../reducers/notificationReducer';
+import anecdoteService from '../services/anecdotes';
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch();
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault();
     const anecdote = event.target.anecdote.value.trim();
     event.target.anecdote.value = '';
     if (anecdote) {
-      dispatch(createAnecdote(anecdote));
-      dispatch(setNotification(`you created '${anecdote}'`));
+      const newAnecdote = await anecdoteService.createNew(anecdote);
+      console.log(newAnecdote)
+      dispatch(createAnecdote(newAnecdote));
+      dispatch(setNotification(`you created '${newAnecdote}'`));
     }
   };
 
--- Part 6 - Advanced state management/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ Part 6 - Advanced state management/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,25 +1,8 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit';
-
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-];
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: nanoid(),
-    votes: 0,
-  };
-};
+import { createSlice } from '@reduxjs/toolkit';
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
-  initialState: anecdotesAtStart.map(asObject),
+  initialState: [],
   reducers: {
     vote(state, action) {
       return state.map((a) =>
@@ -27,10 +10,13 @@ const anecdoteSlice = createSlice({
       );
     },
     createAnecdote(state, action) {
-      return state.concat(asObject(action.payload));
+      return state.concat(action.payload);
+    },
+    setAnecdotes(state, action) {
+      return action.payload;
     },
   },
 });
 
-export const { vote, createAnecdote } = anecdoteSlice.actions;
+export const { vote, createAnecdote, setAnecdotes } = anecdoteSlice.actions;
 export default anecdoteSlice.reducer;
