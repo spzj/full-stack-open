be701b8407cb2824b136042486c11e435d9f4e6c
spzj <54446335+spzj@users.noreply.github.com>
2024-06-27 19:53:47 +0800

    chore(bloglist): update initial blogs link

--- Part 4 - Testing Express servers_user administration/bloglist/utils/initial_blogs.js
+++ Part 4 - Testing Express servers_user administration/bloglist/utils/initial_blogs.js
@@ -20,7 +20,7 @@ const initialBlogs = [
   {
     title: 'First class tests',
     author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html',
     likes: 10,
   },
   {
